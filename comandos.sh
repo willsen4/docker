2690c453933679e91c2c7f5c48fed5411e9f49fd
willsen4 <willsenaa@hotmail.com>
2025-08-03 21:09:22 -0300

    update README and add comandos.sh script

--- comandos.sh
+++ comandos.sh
@@ -1,58 +1,43 @@
-#Instalando no Ubuntu ou Debian
+# Lista Diretorio
+ls
+# Lista permissões do diretório
+ls -ld
 
-#Atualizar os pacotes existentes:
-Bash
-sudo apt update
-sudo apt upgrade -y
+# Lista log
+docker logs nome do conteiner
+# exemplo: 
+docker logs mysql-app
 
-#Instalar pacotes pré-requisitos para Docker:
-Bash
-sudo apt install ca-certificates curl gnupg lsb-release -y
+# Cria diretório
+mkdir nome_do_diretorio
+# exemplo: 
+mkdir mysql-docker
+# se der permissão negada só entra como sudo su
 
-#Adicionar a chave GPG oficial do Docker:
-Bash
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# Remove diretorio
+rm nome_do_diretorio -Rf
+# exemplo: 
+rm mysql-docker -Rf
 
-#Se estiver usando Debian, troque ubuntu por debian no comando acima.
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# Entra na pasta do teampass
+cd mysql-docker
 
-#Adicionar o repositório Docker ao APT sources:
-Bash
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Reinicia o serviço
+docker compose restart nome conteiner
+# exemplo: 
+docker compose restart mysql-app
 
-#Novamente, se for Debian, troque ubuntu por debian no caminho do URL.
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Remove o conteineres
+docker compose down
 
-#Atualizar a lista de pacotes novamente:
-Bash
-sudo apt update
+# Recriar os contêineres
+docker compose up -d
 
-#Instalar o Docker Engine:
-Bash
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# Forçar a recriar os conteineres
+docker compose up -d --force-recreate
 
-#Verificar a instalação do Docker:
-Bash
-sudo docker run hello-world
-
-#Se a instalação foi bem-sucedida, você verá uma mensagem informando que o Docker está funcionando corretamente.
-#Gerenciar o Docker como um Usuário Não-root (Opcional, mas Recomendado)
-#Por padrão, você precisa usar sudo para executar comandos Docker. Para evitar isso e tornar o uso mais conveniente, você pode adicionar seu usuário ao grupo docker.
-
-#Adicionar seu usuário (geralmente sua_conta_de_usuário) ao grupo docker:
-Bash
-sudo usermod -aG docker $USER
-
-#Substitua $USER pelo seu nome de usuário se você não estiver logado como o usuário padrão.
-
-#Aplicar as mudanças: Para que as mudanças tenham efeito, você precisará sair da sessão SSH e fazer login novamente, ou simplesmente reiniciar a VM:
-Bash
-exit
-
-#Depois de reconectar via SSH, você poderá executar comandos docker sem sudo.
+# Acessar o Shell no conteiner
+docker exec -it nome do conteiner bash
+# exemplo: 
+docker exec -it glpi-app bash
+docker exec -it mysql-app mysql -uroot -p
